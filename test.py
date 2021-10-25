acd7c0d98ea742e3e379dfeba4fc47bc3bc90930
Magdalena Biskup <madziabiskup89@gmail.com>
2021-10-25 15:10:53 +0200

    Adding the function welcomeUser

---
+++ test.py
@@ -0,0 +1,9 @@
+def welcomeUser(user: str):
+    """
+    The function returns the user's greeting
+    welcomeUser(userName)
+    """
+    message = "Welcome " + user + "!"
+    return message
+
+print(welcomeUser("mbism"))
