42c69ba250be361e6216ab9417d6dbf1f86a0b41
Magdalena Biskup <madziabiskup89@gmail.com>
2021-10-25 18:44:32 +0200

    Using f-string syntax

--- test.py
+++ test.py
@@ -3,7 +3,7 @@ def welcomeUser(user: str):
     The function returns the user's greeting
     welcomeUser(userName)
     """
-    message = "Welcome " + user + "!"
+    message = f"Welcome {user}!"
     return message
 
 print(welcomeUser("mbism"))
