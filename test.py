d4d6d36a8ba2c9dd09d7c9cab870047cdc3d8b9a
Amadeusz Masny <Amadeusz.Masny1@ibm.com>
2021-10-26 09:44:35 +0200

    Revert "Update test.py"
    This reverts commit abf1bc2c7b7aa8e65bafcf570284ff10898fcebf, reversing
    changes made to e184eefe3f6c5bbecedae3a933a94b40b64e330d.

--- test.py
+++ test.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 class Cars():
     def __init__(self, name: str, year: int) -> None:
         self.name = name
@@ -16,14 +15,3 @@ car_one = Cars("BWM", 2021)
 print(car_one.info())
 
 car_one.get_name()
-=======
-def welcomeUser(user: str):
-    """
-    The function returns the user's greeting
-    welcomeUser(userName)
-    """
-    message = f"Welcome {user}!"
-    return message
-
-print(welcomeUser("mbism"))
->>>>>>> main
